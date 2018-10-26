612eeeea3e2f07dddb8c5965ee2a0f853dfa56e3
Fernando Arévalo <fernando.arevalo9311@gmail.com>
2018-10-25 20:58:32 -0500

    Added mLab datasource

--- app/config/config.js
+++ app/config/config.js
@@ -9,6 +9,8 @@ let urlDB;
 
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/polizas-seguro';
+} else {
+    urlDB = 'mongodb://admin:admin1234@ds027145.mlab.com:27145/sffst'
 }
 
 process.env.URLDB = urlDB;
